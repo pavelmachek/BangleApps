6c3657927279460096b98b683b3a2e6b9679f50f
Pavel Machek <pavel@ucw.cz>
2023-03-03 22:44:45 +0100

    Remove unneeded code from scicalc.

--- apps/waypoint_editor/app.js
+++ apps/waypoint_editor/app.js
@@ -1,93 +1,9 @@
-/* Thanks to scicalc and pinsafe apps from BangleApps repository */
+/* Thanks to pinsafe apps from BangleApps repository */
 
 var Layout = require("Layout");
 
 const W = g.getWidth();
 const H = g.getHeight();
-
-const dispH = H/5;
-const butH = H-dispH;
-
-const buttons = [[['7', '8', '9'],
-                  ['4', '5', '6'],
-                  ['1', '2', '3'],
-                  ['<', '0', '.']],
-                 [[' ', ' ', ' '],
-                  [' ', ' ', ' '],
-                  [' ', ' ', ' '],
-                  [' ', ' ', ' ']],
-                 [[' ', ' ',  ' '],
-                  [' ', ' ', ' '],
-                  [' ', ' ', ' '],
-                  [' ', ' ', ' ']
-                 ]];
-
-var curPage = 0;
-var inputStr = '';
-var memory = '';
-var qResult = false;
-
-function drawPage (p) {
-  g.clearRect(0, dispH, W-1, H-1);
-  g.setFont('Vector', butH/5).setFontAlign(0, 0, 0).setColor(g.theme.fg);
-  for (x=0; x<3; ++x)
-    for (y=0; y<4; ++y)
-      g.drawString(buttons[p][y][x], (x+0.5)*W/3, dispH+(y+0.7)*butH/4);
-  g.setColor(0.5, 0.5, 0.5);
-  for (x=1; x<3; ++x) g.drawLine(x*W/3, dispH+0.2*butH/4-2, x*W/3, H-1);
-  for (y=1; y<4; ++y) g.drawLine(0, dispH+(y+0.2)*butH/4, W-1, dispH+(y+0.2)*butH/4);
-  g.setColor(g.theme.fg).drawLine(0, dispH+0.2*butH/4-2, W-1, dispH+0.2*butH/4-2);
-}
-
-function updateDisp(s, len) {
-  var fh = butH/5; 
-  if (s.toString().length>len) s = s.toString().substr(0,len);
-  g.setFont("Vector", butH/5).setColor(g.theme.fg).setFontAlign(1, 0, 0);
-  while (g.stringWidth(s) > W-1) {
-    fh /= 1.05;
-    g.setFont("Vector", fh);
-  }
-  g.clearRect(0, 0, W-1, dispH-1).drawString(s, W-2, dispH/2);
-  g.setColor(g.theme.fg).drawLine(0, dispH+0.2*butH/4-2, W-1, dispH+0.2*butH/4-2);
-}
-
-function processInp (s) {
-  return s;
-}
-
-function compute() {
-  var res;
-  updateDisp(inputStr, 19);
-}
-
-function touchHandler(e, d) {
-  var x = Math.floor(d.x/(W/3));
-  var y = Math.floor((d.y-dispH-0.2*butH/4)/(butH/4));
-  var c = buttons[curPage][y][x];
-  if (c=="=") { // do the computation
-    compute();
-    return;
-  }
-  else if (c=="<" && inputStr.length>0) inputStr = inputStr.slice(0, -1); // delete last character
-  else inputStr += c;
-  
-  qResult = false;
-  updateDisp(inputStr, 32);
-}
-
-function swipeHandler(e,d) {
-  curPage -= e;
-  if (curPage>buttons.length-1) curPage = 0;
-  if (curPage<0) curPage = buttons.length-1;
-  drawPage(curPage);
-}
-
-function getCoordinates() {
-  Bangle.on("touch", touchHandler);
-  Bangle.on("swipe", swipeHandler);
-  g.clear();
-  drawPage(curPage);
-}
 
 var wp = require('Storage').readJSON("waypoints.json", true) || [];
 
