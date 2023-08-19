bb8c7e8603d847b044d17943b1411bd71f79dc37
Pavel Machek <pavel@ucw.cz>
2023-08-19 20:05:02 +0200

    Use flash, not fork.

--- apps/widbat/widget.js
+++ apps/widbat/widget.js
@@ -26,16 +26,19 @@
     var s = 39;
     var x = this.x, y = this.y;
     g.reset();
-    if (Bangle.isCharging()) {
-      g.setColor("#0f0").drawImage(atob("DhgBHOBzgc4HOP////////////////////3/4HgB4AeAHgB4AeAHgB4AeAHg"),x,y);
-      x+=16;
-    }
     g.setColor(g.theme.fg).fillRect(x,y+2,x+s-4,y+21).clearRect(x+2,y+4,x+s-6,y+19).fillRect(x+s-3,y+10,x+s,y+14);
     var battery = E.getBattery();
     if(battery < 20) {g.setColor("#f00");}
     else if (battery < 50) {g.setColor("#ff0");}
     else {g.setColor("#0f0");}
     g.fillRect(x+4,y+6,x+4+battery*(s-12)/100,y+17);
+    if (1 || Bangle.isCharging()) {
+      let flash = [x+5,y+10, x+18,y+9, x+20,y+4, x+s-5,y+10,
+                   x+22,y+11, x+20,y+15];
+      g.setColor(g.theme.fg).fillPoly(flash);
+      x+=16;
+    }
+      
   }};
   setWidth();
 })()
