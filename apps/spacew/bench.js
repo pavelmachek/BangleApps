ae9a34a25b0bf881189314e5ca9dceadc5a533d5
Pavel Machek <pavel@ucw.cz>
2023-09-08 10:56:23 +0200

    Add line benchmark.

--- apps/spacew/bench.js
+++ apps/spacew/bench.js
@@ -1,15 +1,41 @@
 R = Bangle.appRect;
 
-function redraw() {
-  print("Benchmark");
+function introScreen() {
+  g.reset().clearRect(R);
+  g.setColor(0,0,0).setFont("Vector",25);
+  g.setFontAlign(0,0);
+  g.drawString("Benchmark", 85,35);
+  g.setColor(0,0,0).setFont("Vector",18);
+  g.drawString("Press button", 85,55);
 }
-
+function lineBench() {
+  for (let i=0; i<1000; i++) {
+    let x1 = Math.random() * 160;
+    let y1 = Math.random() * 160;
+    let x2 = Math.random() * 160;
+    let y2 = Math.random() * 160;
+    
+    g.drawLine(x1, y1, x2, y2);
+  }
+}
+function runBench(b) {
+  b();
+}
+function redraw() {
+  g.reset().clearRect(R);
+  g.setColor(0,0,0).setFont("Vector",25);
+  g.setFontAlign(0,0);
+  g.drawString("Running", 85,35);
+  g.setColor(0,0,0).setFont("Vector",18);
+  g.drawString("Press button", 85,55);
+  
+  runBench(lineBench);
+}
 function showMap() {
   g.reset().clearRect(R);
   redraw();
   emptyMap();
 }
-
 function emptyMap() {
   Bangle.setUI({mode:"custom",drag:e=>{
       g.reset().clearRect(R);
@@ -27,16 +53,6 @@ function emptyMap() {
 
 const st = require('Storage');
 const hs = require('heatshrink');
-
-function introScreen() {
-  g.reset().clearRect(R);
-  g.setColor(0,0,0).setFont("Vector",25);
-  g.setFontAlign(0,0);
-  g.drawString("Benchmark", 85,35);
-  g.setColor(0,0,0).setFont("Vector",18);
-  g.drawString("Press button", 85,55);
-  g.drawString("(hello)");
-}
 
 introScreen();
 emptyMap();
